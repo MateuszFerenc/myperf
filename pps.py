05391947a631629b9fe84b1a84970d437d4051d0
mferenc <mateoferenc@gmail.com>
2023-04-21 16:33:53 +0200

    Perf tests repo initial commit Now pps.py can read and interpret the Perf data and plot them onto graph Graph of Perf overhead will be added

---
+++ pps.py
@@ -0,0 +1,68 @@
+#!/bin/python3
+
+from subprocess import check_output
+from statistics import mean, stdev 
+import matplotlib.pyplot as plotter
+from datetime import datetime
+from os.path import join as pjoin
+from os import mkdir
+import argparse
+
+def analyze_perf_script(ignore, tracepoints, data):
+	lines = data.strip().split("\n")
+
+	values = []
+	ignored = 0
+	for i in range(len(lines)):
+		time_, event_ = lines[i].split()
+		time_, event_ = float(time_[:-1].replace(" ", "")), event_[:-1].replace(" ", "")
+		if event_ in ignore:
+			ignored += 1
+			continue
+		values.append(round(time_ * 1000, 4))
+	_min = round(min(values), 2)
+	_max = round(max(values), 2)
+	_avg = round(mean(values), 2)
+	_stdev = round(stdev(values), 2)
+	
+	return (_min, _max, _avg, _stdev, values, ignored)
+	
+if __name__ == "__main__":
+	parser =  argparse.ArgumentParser(description="Script for reading, analysis and displaying Perf script data (2023) Mateusz Ferenc")
+	parser.add_argument("-i", "--ignore", type=str, help="List of tracepoints, separated by space, to ignore [ format: probe_<file-name>:<tracepoint-name> ]")
+	parser.add_argument("-t", "--tracepoints", type=str, help="List of tracepoints, separated by space, to track [ format: probe_<file-name>:<tracepoint-name> ]")
+	parser.add_argument("-f", "--file", type=str, help="File with data dumped from Perf script")
+	args = parser.parse_args()
+	data = None
+	if args.tracepoints is not None and args.file is not None:
+		with open(args.file, "r") as file_data:
+			data = file_data.read()
+		result = analyze_perf_script(ignore=(list(args.ignore.split(" ")) if args.ignore is not None else []), tracepoints=list(args.tracepoints.split(" ")), data=data)
+		print(f"Minumum: {result[0]} ms\nMaximum: {result[1]} ms\nAverage: {result[2]} ms\nStandard deviation: {result[3]} ms\nIgnored tracepoints: {result[5]}")
+		plotter.plot(result[4], color='r', label='funcB')
+		plotter.xticks(range(0, len(result[4])))
+		plotter.xlabel('tracepoint no.', weight='light', style='italic')
+		plotter.ylabel('time [ms]', weight='light', style='italic')
+		plotter.title(f"{args.ignore} {args.tracepoints} exectution time", weight='bold')
+		plotter.legend()
+		plotter.grid('on', linestyle=':', linewidth=0.5)
+	
+		results_dir = "results"
+	
+		try:
+			mkdir(results_dir)
+		except FileExistsError:
+			pass
+	
+		now_time = datetime.now()
+	
+		file_name = f"funcB_diff_plot_{now_time.strftime('%H%M%S_%d%m%y')}.png"
+		save_path = pjoin(results_dir, file_name) if results_dir is not None else file_name
+
+		try:
+			plotter.savefig(save_path, dpi=500)
+		except FileExistsError:
+			pass
+	
+	else:
+		print("Error:\nNo tracepoints to analyze.\t\t Aborting...")
---
+++ test.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <unistd.h>
+
+void funcB(int l){
+   	usleep(1000*l);
+   	printf("l=%d\n", l);
+}
+
+int main(){
+	for (int i = 0; i < 20; i++){
+		printf("i=%d\n", i);
+		if(i>=10)
+			funcB(1000);
+		else
+			funcB(50);
+   	}
+   	return 0;
+}
